d74e3a47416616b12c65551b3f5704d7f054a488
shashwatsinha10 <shashwatsinha050@gmail.com>
2020-08-04 23:06:12 +0530

    Quiz - Basic1

--- app_1_QUIZ/lib/main.dart
+++ app_1_QUIZ/lib/main.dart
@@ -1,3 +1,10 @@
-main(){
-  
+import 'package:flutter/material.dart';
+main() {
+  runApp(MyApp());
 }
+
+class MyApp extends StatelessWidget{
+  Widget build (BuildContext context) {
+    return MaterialApp(home: Text('Text is alo a widget'));
+  }
+}  
